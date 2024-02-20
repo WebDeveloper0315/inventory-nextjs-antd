692dfc0c0454f41f7211ec8afa4f164043603909
Blackghost <139863785+WebDeveloper0315@users.noreply.github.com>
2024-02-20 13:22:58 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
-const DB_URI="mongodb://127.0.0.1:27017/inventory-nextjs-antd"
+// const DB_URI="mongodb://127.0.0.1:27017/inventory-nextjs-antd"
+const DB_URI="mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/inventory-management-nextjs-antd"
 const JWT_SECRET="blackghost"
 
 module.exports = {
