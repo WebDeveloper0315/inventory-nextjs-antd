a6f70de68a024ce98210e7e4bfcf469d4600e1b5
blackghost <blackghost1503@gmail.com>
2024-05-09 14:53:40 +0700

    updated the not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,36 +1,25 @@
 "use client";
 import React from "react";
-import { Button } from "antd";
-import { useRouter } from "next/navigation";
 
 function Custom404() {
-  const divStyle: React.CSSProperties = {
-    background: `url('/image/404.jpg')`,
-    backgroundSize: "cover",
-    color: "white", // Set text color to ensure readability
-    height: "100vh",
-  };
-
-  const buttonContainerStyle: React.CSSProperties = {
-    position: "absolute",
-    top: "60%",
-    left: "35%",
-    
-  };
-
-  const router = useRouter();
   return (
-    <div style={divStyle}>
-      <div style={buttonContainerStyle}>
-        <Button
-          type="primary"
-          size="large"
-          onClick={() => {
-            router.push("/");
-          }}
-        >
-          Go to Home
-        </Button>
+    <div className="h-full w-full items-center justify-center">
+      <div className="text-center">
+        <p className="text-base font-semibold text-indigo-600">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
+          Page not found
+        </h1>
+        <p className="mt-6 text-base leading-7 text-gray-400">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <a
+            href="#"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Go back home
+          </a>
+        </div>
       </div>
     </div>
   );
